a3751b08675ea071db100640d26d95fe99e2bb00
Kim Sang <kimsang@gmail.com>
2018-08-11 09:25:50 +0700

    Secondary commit

--- App/Container/Home/index.js
+++ App/Container/Home/index.js
@@ -7,7 +7,7 @@ export default class Home extends Component {
       <Container>
        
         <Content>
-        <Header />
+       
         <Banner />
    
         </Content>
--- App/Navigation/index.js
+++ App/Navigation/index.js
@@ -1,9 +1,13 @@
-
-import {AppRegistry} from 'react-native';
-import React, { Component } from 'react'
-
-import { createBottomTabNavigator } from 'react-navigation';
+import React from 'react';
+import { 
+    createStackNavigator, 
+    createDrawerNavigator, 
+} from 'react-navigation';
+import {
+    createMaterialBottomTabNavigator
+} from 'react-navigation-material-bottom-tabs';
 // import App from './App';
+import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
 
 import HomeScreen from './HomeScreen';
 import InfoScreen from './InfoScreen';
@@ -107,33 +111,132 @@ import CloudScreen from './CloudScreen';
 // });
 
 
-export default createBottomTabNavigator({
-    Home: {
-        screen: HomeScreen,
-            navigationOptions: {
-                title: 'Home',
-            },
-    },
-    Info: {
-        screen: InfoScreen,
-                navigationOptions: {
-                title: 'Info',
-            },
-    },
-    Settings: {
-        screen: SettingsScreen,
-                    navigationOptions: {
+// export default createBottomTabNavigator({
+//     Home: {
+//         screen: HomeScreen,
+//             navigationOptions: {
+//                 title: 'Home',
+//             },
+//     },
+//     Info: {
+//         screen: InfoScreen,
+//                 navigationOptions: {
+//                 title: 'Info',
+//             },
+//     },
+//     Settings: {
+//         screen: SettingsScreen,
+//                     navigationOptions: {
                 
-            },
-    },
-    Cloud: {
-        screen: CloudScreen,
+//             },
+//     },
+//     Cloud: {
+//         screen: CloudScreen,
                 
-                    navigationOptions: {
+//                     navigationOptions: {
                 
-            },
-        },       
-}, {
+//             },
+//         },       
+// }, {
       
             
+// });
+
+
+const HomeStackNavigator = createStackNavigator({
+     Home: {
+        screen: HomeScreen,
+        navigationOptions: {
+            headerTintColor: '#ffff',
+            headerStyle:{
+                backgroundColor:'#1d2831',
+                elevation: 0,
+                showdowOpacity:0,
+            }   
+        }
+    }
+})
+const InfoStackNavigator = createStackNavigator({
+     Info: {
+        screen: InfoScreen,
+        navigationOptions: {
+            headerTintColor: '#ffff',
+            headerStyle:{
+                backgroundColor:'#1d2831',
+                elevation: 0,
+                showdowOpacity:0,
+            }   
+        }
+    }
+})
+const SettingsStackNavigator = createStackNavigator({
+    Settings: {
+        screen: SettingsScreen,
+        navigationOptions: {
+            headerTintColor: '#ffff',
+            headerStyle:{
+                backgroundColor:'#1d2831',
+                elevation: 0,
+                showdowOpacity:0,
+            }   
+        }
+    }
+})
+
+const CloudStackNavigator = createStackNavigator({
+        Cloud: {
+        screen: CloudScreen,
+        navigationOptions: {
+            headerTintColor: '#ffff',
+            headerStyle:{
+                backgroundColor:'#1d2831',
+                elevation: 0,
+                showdowOpacity:0,
+            }   
+        }
+    },
+   
 });
+
+
+export default createMaterialBottomTabNavigator(
+    {
+        Home: HomeStackNavigator,
+        Info: InfoStackNavigator,
+        Settings: SettingsStackNavigator,
+        Cloud: CloudStackNavigator
+    },
+    {
+        initialRouteName: 'Home',
+        shifting: true,
+        navigationOptions: ({ navigation }) => ({
+            tabBarIcon: ({ focused, tintColor }) => {
+                const { routeName } = navigation.state;
+
+                let iconName = 'home';
+                if (routeName === 'Home') {
+                    iconName = 'home';
+                } else if (routeName === 'Noti') {
+                    iconName = 'notifications';
+                } else if (routeName === 'Menu') {
+                    iconName = 'menu';
+                }else if (routeName === 'Category') {
+                    iconName = 'menu';
+                }
+
+                return <MaterialIcon name={iconName} size={25} color={tintColor} />;
+            },
+        }),
+    }
+)
+
+
+// export default createDrawerNavigator(
+//     {
+//         Home: BottomNavigator
+//     },
+//     {
+//         contentComponent: MenuScreen
+//     }
+// );
+
